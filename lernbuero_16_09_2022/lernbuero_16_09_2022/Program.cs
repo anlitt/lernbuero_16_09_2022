ceb4008395cf7fd55631d5cce9658b97a88b89a6
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-16 08:32:43 +0200

    arrays 16 09 2022 #1

--- lernbuero_16_09_2022/lernbuero_16_09_2022/Program.cs
+++ lernbuero_16_09_2022/lernbuero_16_09_2022/Program.cs
@@ -11,22 +11,27 @@ namespace lernbuero_16_09_2022
         static void Main(string[] args)
         {
             {
-                //array hat 5 stellen aber keine Zahlen dafür definiert, daher alle 0
-                int[] zahlen = new int[5];
-
-                //in dem array befinden sich die Zahlen 26, 8, 12, 3 und 6. Also nicht definiert wie viele Zahlen sonder vor allem welche
-                int[] zahlen2 = new int[] { 26, 8, 12, 3, 6 };
-                for (int i = 0; i < zahlen.Length; i++)
+                //sagt wieder das 13 plätze im string array sind 
+                string[] namen = new string[13];
+                //sagt welche namen drin sind und weist dementspreechend viele plätze zu
+                string[] namen2 = new string[] {"lukas", "jonas", "mona", "marie", "philip","jan",
+                "erwin", "leon" ,"marcello", "robin", "johannes", "andreas",
+                "robert"
+                };
+                
+                for(int i = 0; i < namen2.Length; i++)
                 {
-                    Console.WriteLine("Gebe mir eine Zahl für das Array!");
-                    zahlen[i] = Convert.ToInt32(Console.ReadLine());
+                    namen2[i] += " 123";
+                    Console.WriteLine(namen2[i]);
                 }
-                Console.Write("[");
-                for (int i = 0; i < zahlen2.Length; i++)
+                
+                /*foreach(string trainee in namen2)
                 {
-                    Console.Write($" {zahlen[i]} / ");
-                }
-                Console.Write("]");
+                    //kann Trainee nicht als Michael funktionieren; ist technisch nicht möglich
+                    trainee = "Michael";
+                    Console.WriteLine(trainee);
+                }*/
+               
                 Console.ReadLine();
 
             }
