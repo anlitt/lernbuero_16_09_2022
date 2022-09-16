fed5162f04b7c95c5f7c70f009895d7b6ad61713
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-16 08:19:46 +0200

    arrays #1

---
+++ lernbuero_16_09_2022/lernbuero_16_09_2022/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lernbuero_16_09_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //array hat 5 stellen aber keine Zahlen dafür definiert, daher alle 0
+                int[] zahlen = new int[5];
+
+                //in dem array befinden sich die Zahlen 26, 8, 12, 3 und 6. Also nicht definiert wie viele Zahlen sonder vor allem welche
+                int[] zahlen2 = new int[] { 26, 8, 12, 3, 6 };
+                for (int i = 0; i < zahlen.Length; i++)
+                {
+                    Console.WriteLine("Gebe mir eine Zahl für das Array!");
+                    zahlen[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.Write("[");
+                for (int i = 0; i < zahlen2.Length; i++)
+                {
+                    Console.Write($" {zahlen[i]} / ");
+                }
+                Console.Write("]");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
